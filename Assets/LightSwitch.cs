cd980476de60fd50a6d3fcb502f004c4a4782e21
Atte <atte.kantola@student.oulu.fi>
2026-01-25 12:28:00 +0200

    Update LightSwitch.cs

--- Assets/LightSwitch.cs
+++ Assets/LightSwitch.cs
@@ -1,22 +1,19 @@
-using UnityEngine;
+using UnityEngine; 
 
-public class LightSwitch : MonoBehaviour
-{
-    public Light light;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        light = GetComponent<Light>();
+public class LightSwitch : MonoBehaviour 
+{ 
+	private Light lightSource; 
 
-        if (Input.GetKeyDown("SPACE"))
-        {
-            light.color = Color.blue;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start() 
+	{ 
+		lightSource = GetComponent<Light>(); 
+	} 
+	
+	void Update() 
+	{
+	    if (Input.GetKeyDown(KeyCode.Space)) 
+		{ 
+        lightSource.color = Color.blue; 
+        } 
+    } 
 }
